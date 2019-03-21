be5f4ffa2948e9da46213daebe0ffdc91c5d7af0
201631053 <hubot@githhub@com>
2019-03-21 17:40:21 +0900

    오르락 내리락

--- Button_Fun_02/ViewController.swift
+++ Button_Fun_02/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Button_Fun_02
-//
-//  Created by D7703_04 on 2019. 3. 21..
-//  Copyright © 2019년 test. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
